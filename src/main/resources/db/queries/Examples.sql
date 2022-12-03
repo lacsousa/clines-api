75956432525fbb58bfd60a680ad6879915daac1e
Luciano Cordeiro <lacsousa@gmail.com>
2022-12-03 17:04:22 -0300

    Creating a new test and creating API calls

--- src/main/resources/db/queries/Examples.sql
+++ src/main/resources/db/queries/Examples.sql
@@ -1,29 +1,38 @@
 -- Auto-generated SQL script #202210121338
-INSERT into aircraft_models (description)
+
+INSERT INTO public.aircraft_models (description)
+	VALUES ('Carga');
+
+INSERT into public.aircraft_models (description)
 	VALUES ('Comericiais');
+
 	
+select * from public.aircraft_models am;
 
-INSERT INTO aircraft (code,model_id)
-	VALUES ('Boeing 737',1);
+INSERT INTO public.aircraft (code,model_id)
+	VALUES ('Boeing 737',2);
 
 
-INSERT INTO users ("name",email,"password")
+INSERT INTO public.users ("name",email,"password")
 	VALUES ('Luciano Cordeiro','lacs@teste.com','1234567');
 
 
-INSERT INTO users ("name",email,"password")
+INSERT INTO public.users ("name",email,"password")
 	VALUES ('Lívian','livian@teste.com','1234567');
 
 
-INSERT INTO users ("name",email,"password")
+INSERT INTO public.users ("name",email,"password")
 	VALUES ('Líris','liris@teste.com','1234567');
 
 
 INSERT INTO public.locations (country,state,city)
 	VALUES ('55','01','Natal');
 
-INSERT INTO airports (code,location_id)
+INSERT INTO public.airports (code,location_id)
 	VALUES ('NAT',1);
 	
 
--- drop database clines;
+--drop database if exists clines ;
+
+--create database clines;
+
